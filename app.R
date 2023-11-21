b723eb8608bac67a7d4308f543fab2b4b1254474
nmhowes <nmhowes55@gmail.com>
2023-11-21 10:33:07 -0800

    final

---
+++ app.R
@@ -0,0 +1,61 @@
+#shiny app
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(DT)
+library(colourpicker)
+
+vt <- read.csv("vt.csv", stringsAsFactors = FALSE)
+#print(str(vt))
+
+
+ui <- fluidPage(h1("How to find Vancouver trees"),
+                h3("Use this app to find where specific trees are in Vancouver"),
+                titlePanel("Find trees according to diameter, height and location on/off curb"),
+                sidebarLayout(
+                sidebarPanel(
+                  sliderInput("diameterInput", "Diameter", 0, 1000, c(300, 500), post = "m"),
+                  sliderInput("height_range_idInput", "Height Range ID", 0, 10, c(2)),
+                  checkboxGroupInput("curbInput", "Is Tree Located on Curb (Yes = Y or No = N)", choices = c("Y", "N"), selected = "Y")),
+                  colourpicker::colourInput(paste("col", sep="_"), "Choose colour:", "black")),
+                mainPanel(
+                  tabsetPanel(
+                  tabPanel("Plot", plotOutput("coolplot")),
+                  tabPanel("Table",DT::dataTableOutput("results")),
+                  br(), br())
+                ))
+#Feature: Added parameter to plot to allow user to change colour of bars
+#Feature: Put plot and table in separate tabs
+
+server <- function(input, output){
+  filtered <- reactive({
+    if (is.null(input$curbInput)) {
+      return(NULL)
+    }
+    vt %>%
+      filter(diameter >= input$diameterInput[1],
+           diameter <= input$diameterInput[2],
+           height_range_id == input$height_range_idInput,
+           curb %in% input$curbInput
+  )
+  })
+
+  output$coolplot <- renderPlot({
+  ggplot(filtered(), aes(neighbourhood_name)) +
+    geom_bar(fill = input$col)
+
+})
+
+
+  output$results <- DT::renderDataTable({
+  filtered()
+  })
+}
+
+#Feature : Used DT package to create an interactive table
+
+
+shinyApp(ui = ui, server = server)
+
+
+
